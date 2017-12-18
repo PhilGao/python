da6366cc9f7fccf34d4eaacaf29dad63270ce2a8
PhilGao <gao.hao48@foxmail.com>
2017-12-18 12:00:53 +0800

    Generate 200 Code

--- phil.gao/0001.py
+++ phil.gao/0001.py
@@ -7,9 +7,13 @@ def RandomCode(len = 6 , base = string.ascii_lowercase + string.digits ):
 	
 	# str.join() would generate a new string ,   always using _ standing for a variable that would not get used 
 	return ''.join(base[randint(0,len)] for _ in range(len))
-		
-print(RandomCode())
-print(RandomCode(8))
+
+result
+for _ in range(200):
+	result.append(RandomCode(8))
+
+print(type(result))
+print(result)
 
 
 #return ''.join(random.choice(chars) for _ in range(size))  
